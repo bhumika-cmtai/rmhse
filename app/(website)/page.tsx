a1e2b7ee90c9be74ba9df2eece33fc5efd62aaed
bhumikaPandeyCodes <140793724+bhumikaPandeyCodes@users.noreply.github.com>
2025-08-01 11:39:06 +0530

    refactor: simplify homepage structure and update component rendering

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,30 +1,11 @@
-"use client"
-import Hero from './home/Hero'
-import WhatWeDo from './home/WhatWeDo';
-import AboutUs from './home/AboutUs';
-import WhyChooseUs from './home/WhyChooseUs';
-import WhoCanJoin from './home/WhoCanJoin';
-import LegalStatus from './home/LegalStatus';
-import Achievements from './home/Achievements';
-import Testimonials from './home/Testimonials';
-import ContactUs from './home/ContactUs';
+import React from 'react'
 
-
-export default function HomePage() {
-  
-
+const page = () => {
   return (
-    <main className="flex min-h-screen max-w-7xl flex-col items-center justify-center py-20 mx-auto ">
-     
-      <Hero />
-      <WhatWeDo />
-      <AboutUs />
-      <WhyChooseUs />
-      <WhoCanJoin />
-      <Testimonials />
-      <LegalStatus />
-      <Achievements />
-      <ContactUs />
-    </main>
-  );
+    <div>
+      Website
+    </div>
+  )
 }
+
+export default page
